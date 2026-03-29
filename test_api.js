cc8ac499225c775c04fa16b64f41762b4c95e978
Meal App Dev <user@meal-app.com>
2026-03-30 05:09:55 +0600

    fix: remove garbage local test scripts that broke vercel build linting

--- test_api.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function run() {
-  const mess = await prisma.mess.findFirst();
-  console.log("Auto Meal entry:", mess?.autoMealEntry);
-  console.log("Blackouts:", mess?.mealBlackouts);
-}
-run().finally(() => prisma.$disconnect());
--- test_api2.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function run() {
-  const mess = await prisma.mess.findFirst();
-  console.log("Mess records:", mess);
-}
-run().finally(() => prisma.$disconnect());
